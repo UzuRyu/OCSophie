d010a0483c1063517a2b3a4e024371d18e609dbb
UzuRyu <uzuryu@gmail.com>
2023-08-28 22:07:07 +0200

    deleteWork() corrigée

--- FrontEnd/scriptMain.js
+++ FrontEnd/scriptMain.js
@@ -46,49 +46,12 @@ async function renderCat(id) {
         let newFigCap = document.createElement("figcaption");
         newFigImg.setAttribute('src',work.imageUrl.replace("http://localhost:5678", "../Backend"));
         newFigImg.setAttribute('alt',work.title);
-        newFigCap.appendChild(document.createTextNode(work.title));
+        newFigCap.appendChild(document.createTextNode(work.id));
         newFig.append(newFigImg);
         newFig.append(newFigCap);
         contenuGallery.append(newFig);
       }
     });
-  }
-}
-
-async function renderAllWorksModal(){
-  console.log("All");
-  modalGallery.innerHTML = '';  // Nettoyage de la gallerie //   
-  let workJSON = await getAllWorks();  // Obtention des travaux //
-  let i = 1;
-  workJSON.forEach((work) => {
-    // Ecriture des Figures suivi de leur ajout à la gallerie //
-    let newFigDiv = document.createElement("div");
-    let newFig = document.createElement("figure");
-    let newFigImg = document.createElement("img");
-    let newFigCap = document.createElement("figcaption");
-    let newFigDelete = document.createElement("button");
-    let iconDelete = document.createElement("i");
-    newFigDiv.setAttribute('id', "idFig" + i);
-    newFigDiv.setAttribute('class', "figDiv");
-    newFigImg.setAttribute('src',work.imageUrl.replace("http://localhost:5678", "../Backend"));
-    newFigImg.setAttribute('alt',work.title);
-    //newFigDelete.setAttribute('type',"radio");
-    newFigDelete.setAttribute('name',"deleteButt");
-    newFigDelete.setAttribute('class',"deleteBtn");
-    newFigDelete.setAttribute('value',i);
-    iconDelete.setAttribute('class',"fa-solid fa-trash-can")
-    newFigDelete.appendChild(iconDelete);
-    newFigDiv.appendChild(newFig);
-    newFigDiv.appendChild(newFigDelete);
-    newFigCap.appendChild(document.createTextNode("éditer"));
-    newFig.append(newFigImg);
-    newFig.append(newFigCap);
-    modalGallery.append(newFigDiv);
-    i++
-  });
-  let everyDeleteBtn = document.getElementsByClassName("deleteBtn");
-  for(let f = 1;f <= everyDeleteBtn.length; f++){
-    everyDeleteBtn[f].addEventListener("click", deleteWork.bind(this, f));
   }
 }
 
@@ -143,7 +106,6 @@ async function valid(){
 }
 
 renderAllWorks();
-renderAllWorksModal()
 
 // Gestion du "bouton" de Login/Logout //
 document.getElementsByTagName("li")[2].addEventListener("click", () => {
@@ -165,8 +127,48 @@ document.getElementById("filtres").addEventListener("change", () => {
   });
 });
 
+async function renderAllWorksModal(){
+  console.log("All");
+  modalGallery.innerHTML = '';  // Nettoyage de la gallerie //   
+  let workJSON = await getAllWorks();  // Obtention des travaux //
+  let i = 1;
+  workJSON.forEach((work) => {
+    // Ecriture des Figures suivi de leur ajout à la gallerie //
+    let newFigDiv = document.createElement("div");
+    let newFig = document.createElement("figure");
+    let newFigImg = document.createElement("img");
+    let newFigCap = document.createElement("figcaption");
+    let newFigDelete = document.createElement("button");
+    let iconDelete = document.createElement("i");
+    newFigDiv.setAttribute('id', "idFig" + i);
+    newFigDiv.setAttribute('class', "figDiv");
+    newFigImg.setAttribute('src',work.imageUrl.replace("http://localhost:5678", "../Backend"));
+    newFigImg.setAttribute('alt',work.title);
+    newFigDelete.setAttribute('name',"deleteButt");
+    newFigDelete.setAttribute('class',"deleteBtn");
+    newFigDelete.setAttribute('value',i);
+    newFigDelete.addEventListener("click", deleteWork.bind(this, work.id));
+    iconDelete.setAttribute('class',"fa-solid fa-trash-can")
+    newFigDelete.appendChild(iconDelete);
+    newFigDiv.appendChild(newFig);
+    newFigDiv.appendChild(newFigDelete);
+    newFigCap.appendChild(document.createTextNode("éditer"));
+    newFig.append(newFigImg);
+    newFig.append(newFigCap);
+    modalGallery.append(newFigDiv);
+    i++
+  });
+  /*
+  let everyDeleteBtn = document.getElementsByClassName("deleteBtn");
+  for(let f = 1; f <= everyDeleteBtn.length; f++){
+    everyDeleteBtn[f].addEventListener("click", deleteWork.bind(this, f));
+  }
+  */
+}
+
 if(user !== null){
   adminMode();
+  renderAllWorksModal()
 }
 
 document.querySelectorAll("deleteBtn").forEach(a => {
